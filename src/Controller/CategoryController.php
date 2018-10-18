ee0f65f605d8b7c5331058c30e88f78ce9f78b77
Ludovic <ludo.m@live.fr>
2018-10-18 15:54:28 +0200

    Set up the style

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -15,9 +15,11 @@ class CategoryController extends AbstractController
      */
     public function index()
     {
-        return $this->render('category/index.html.twig', [
-            'controller_name' => 'CategoryController',
-        ]);
+        $categories = $this->getDoctrine()
+                           ->getRepository(Category::class)
+                           ->findAll();
+
+        return $this->render('category/displayCategory.html.twig', array("categories" => $categories));
     }
 
     /**
@@ -38,6 +40,49 @@ class CategoryController extends AbstractController
             return $this->redirect($this->generateUrl('category'));
         }
 
-        return $this->render('category/newCategory.html.twig', array('form' => $form->createView()));
+        return $this->render('category/createCategory.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+    * @Route("/modifyCategory/{id}", name="modifyCategory")
+    */
+    public function modifyCategory(Request $request, $id)
+    {
+        $category = $this->getDoctrine()
+                     ->getRepository(category::class)
+                     ->find($id);
+
+         $form = $this->createForm(categoryType::class, $category);
+         $form->handleRequest($request);
+
+         if ($form->isSubmitted() && $form->isValid())
+         {
+             $em = $this->getDoctrine()->getManager();
+             $em->persist($category);
+             $em->flush();
+
+             return $this->redirect($this->generateUrl('category'));
+         }
+
+        return $this->render('task/modifyCategory.html.twig', array('form' => $form->createView()));
+    }
+
+    /**
+     * @Route("/deleteCategory/{id}", name="deleteCategory")
+     */
+    public function deleteCategory($id)
+    {
+        $category = $this->getDoctrine()
+                      ->getRepository(category::class)
+                      ->findAll();
+
+        if ($id != null) {
+            $em = $this->getdoctrine()->getManager();
+            $category = $em->getRepository(category::class)->find($id);
+            $em->remove($category);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('category'));
     }
 }
